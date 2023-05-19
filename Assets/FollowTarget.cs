f0bae85926171231ced8a8db5efbe7bd36fd2c9e
sator <satorsight@gmail.com>
2023-05-19 17:38:27 +0900

    init

--- Assets/FollowTarget.cs
+++ Assets/FollowTarget.cs
@@ -6,11 +6,11 @@ public class FollowTarget : MonoBehaviour
 {
 
     public float turnSpeed = 2;
-    public float viewRange = 60.0f;
     public Camera camera;
 
     void Start()
     {
+        // do it here because its heavy call
         camera = Camera.main;
     }
 
@@ -28,25 +28,23 @@ public class FollowTarget : MonoBehaviour
         horizontalRotation.eulerAngles = new Vector3(horizontalRotation.eulerAngles.x, horizontalRotation.eulerAngles.y, 0f);
         verticalRotation.eulerAngles = new Vector3(verticalRotation.eulerAngles.x, verticalRotation.eulerAngles.y, 0f);
 
+        // rotate horizontally
         transform.rotation *= Quaternion.Slerp(
             Quaternion.identity,
             horizontalRotation,
             Mathf.Abs(turn) * turnSpeed * Time.deltaTime
         );
 
+        // rotate vertically
         transform.rotation *= Quaternion.Slerp(
             Quaternion.identity,
             verticalRotation,
             Mathf.Abs(turnV) * turnSpeed * Time.deltaTime
         );
 
-        //Quaternion res = transform.rotation;
-
         Vector3 eulerRotation = transform.rotation.eulerAngles;
-        //float clampedX = Mathf.Clamp(eulerRotation.x, 20f, 340f);
-
-        //Debug.Log("eulerRotation.x");
-        //Debug.Log(eulerRotation.x);
+        
+        // code below needed to limit camera angle rotation
 
         // 300 - 50
         // 0 - 90, 270 - 360
@@ -54,24 +52,15 @@ public class FollowTarget : MonoBehaviour
 
         if (val > 0 && val < 90)
         {
-            //if (val > 70)
-            //{
-            //    val = 70;
-            //}
             val = Mathf.Clamp(val, 0, 70);
-        } else if (val > 200)
+        }
+        else if (val > 200)
         // 270 - 360 range
         {
-            //if (val < 270)
-            //{
-            //    val = 270;
-            //}
             val = Mathf.Clamp(val, 290, 360);
         }
 
 
         transform.rotation = Quaternion.Euler(val, eulerRotation.y, 0);
-
-        //camera.transform.localEulerAngles = new Vector3(Mathf.Clamp(camera.transform.localEulerAngles.x, -viewRange, viewRange), 0, 0);
     }
 }
--- Assets/GirlMover.cs
+++ Assets/GirlMover.cs
@@ -6,7 +6,6 @@ public class GirlMover : MonoBehaviour
 {
 
     public float speed = 6;
-    public float turnSpeed = 2;
     public float jumpAmount = 10;
     public Animator anim;
     private AnimatorStateInfo currentState;     // 現在のステート状態を保存する参照
@@ -23,84 +22,36 @@ public class GirlMover : MonoBehaviour
     {
         body = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
-        //lookPos = GetComponent<FollowTarget>();
-
-
     }
 
     void Start()
     {
-        // 各参照の初期化
-        //anim = GetComponent<Animator>();
         currentState = anim.GetCurrentAnimatorStateInfo(0);
         previousState = currentState;
         camera = Camera.main;
-
-
-
-
-
     }
 
-// Update is called once per frame
-void Update()
+    // Update is called once per frame
+    void Update()
     {
-
-        //PreventChildRotation();
-
-        //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,
-        //           Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
-
-
-        //transform.center = Camera.main.transform.position;
-
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        // basic movement vector, need to be separate without speed so we can normalize it
         Vector3 moveVec = new Vector3(horizontal, 0, vertical);
         moveVec.Normalize();
 
+        // after normilize adjusting speed to make frame independent
         Vector3 spedUpVec = new Vector3(moveVec.x * speed * Time.deltaTime, 0, moveVec.z * speed * Time.deltaTime);
-
+        // make it dependent on camera rotation
         Vector3 movement = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0) * spedUpVec;
-        //movement.Normalize();
 
         Quaternion camDirection = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0);
-
-
-        //Vector3 anotherVec = new Vector3(horizontal , 0, vertical);
-        //anotherVec.Normalize();
-        //Vector3 someVec = new Vector3(horizontal * speed * Time.deltaTime, 0, vertical * speed * Time.deltaTime);
-        //Vector3 movement = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0) * someVec;
-
-
-
-
-
-        //transform.Translate(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
-
         transform.Translate(movement, Space.World);
 
-        //Vector3 movement2 = new Vector3(movement.x, movement.y, movement.z);
-
+        // Here we need to rotate character with movement, but take in account that if we moving backwards rotation should be different
         if (movement != Vector3.zero)
         {
-
-            //Debug.Log("movement");
-            //Debug.Log(movement);
-            //Debug.Log("camera.transform.forward");
-            //Debug.Log(camera.transform.forward);
-
-            ////Vector3 fwd = transform.forward;
-
-            //// use the 0.5f through arccos for the 30-degree demarcation
-            //// angle past which we will consider you moving backwards.
-            //if (Vector3.Dot(movement, camera.transform.forward) < -0.5f)
-            //{
-            //    movement = -movement;
-            //    // walking backwards
-            //    //gameObject.transform.forward = -move;
-            //}
 
             Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
             if (vertical > -0.01f)
@@ -111,22 +62,10 @@ void Update()
                 toRotation = Quaternion.LookRotation(-movement, Vector3.up);
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.deltaTime);
             }
-
-
-            //transform.forward = movement;
             
         }
 
-        //float turn = Input.GetAxis("Mouse X");
-
-
-        //direction = new Vector3(horizontal, 0, vertical);
-
-        //direction = direction.normalized;
-        //body.velocity = Camera.main.transform.TransformDirection(direction) * speed * Time.deltaTime;
-
-
-
+        // I dont know what this code does and how its used
 
         // ↑キー/スペースが押されたら、ステートを次に送る処理
         if (Input.GetKeyDown("up") || Input.GetButton("Jump"))
@@ -166,78 +105,27 @@ void Update()
             }
         }
 
-        /* if (speed > 0) {
-             if (horizontal > 0.1)
-             {
-                 anim.SetFloat("Direction", 1);
-             }
-             else if (horizontal < -0.1)
-             {
-                 anim.SetFloat("Direction", -1);
-             }
-             else
-             {
-                 anim.SetFloat("Direction", 0);
-             }
-         }*/
-
-        // Vector3 movement = new Vector3(horizontal, vertical);
-        /*
-        if (turn > 0)
-        {
-            anim.SetFloat("Direction", 1);
-        } else
-        {
-            anim.SetFloat("Direction", -1);
-
-        }*/
-
-        //transform.position += movement * Time.deltaTime;
-        //Debug.Log(lookPos);
-        //Debug.Log(Find("LookPos").GetComponent<FollowTarget>());
-        //transform.rotation = lookPos.transform.rotation;
-
-        //transform.rotation *= Quaternion.Slerp(
-        //    Quaternion.identity, Quaternion.LookRotation(turn < 0 ? Vector3.left : Vector3.right), Mathf.Abs(turn) * turnSpeed * Time.deltaTime
-        //    );
-        float h = Input.GetAxis("Horizontal");              // 入力デバイスの水平軸をhで定義
-        float v = Input.GetAxis("Vertical");                // 入力デバイスの垂直軸をvで定義
-        anim.SetFloat("Speed", v);                          // Animator側で設定している"Speed"パラメタにvを渡す
-        anim.SetFloat("Direction", h);                      // Animator側で設定している"Direction"パラメタにhを渡す
+        // end of code I didn't write
 
 
+        // This defines animations transitions
+        anim.SetFloat("Speed", vertical);
+        anim.SetFloat("Direction", horizontal);
 
-
-
+        // inside we detect if jump key is pressed and if character can jump
         JumpWhenNeeded();
 
         if (isGrounded())
         {
             Debug.Log("isGrounded()");
             Debug.Log(isGrounded());
-
         }
 
-        //if (direction != Vector3.zero)
-        //{
-
-        //    HandleRotation();
-        //}
-
-        //MoveInDirectionOfInput();
-
-        //anim.SetFloat("Speed", vertical + horizontal);
-    }
-
-    private void PreventChildRotation()
-    {
-        lookPos.transform.rotation = Quaternion.Euler(0, -transform.rotation.y, 0);
     }
 
 
     private void JumpWhenNeeded()
     {
-
 
         if (Time.time > jumpStarts + 0.5f)
         {
@@ -250,13 +138,6 @@ void Update()
 
         }
 
-
-
-
-
-
-
-
         bool isJumping = anim.GetBool("Jump");
 
         if (isJumping == false)
@@ -267,50 +148,13 @@ void Update()
             {
                 anim.SetBool("Jump", true);
                 StartCoroutine(Jump());
-                //anim.SetBool("Jump", true);
-                //yield return new WaitForSeconds(1);
-                //body.AddForce(Vector3.up * jumpAmount, ForceMode.Impulse);
-                //jumpStarts = Time.time;
             }
         }
 
         
     }
 
-    //public void MoveInDirectionOfInput()
-    //{
-    //    Vector3 dir = Vector3.zero;
-
-    //    dir.x = Input.GetAxis("Horizontal");
-    //    dir.z = Input.GetAxis("Vertical");
-
-    //    Vector3 camDirection = Camera.main.transform.rotation * dir; //This takes all 3 axes (good for something flying in 3d space)    
-    //    Vector3 targetDirection = new Vector3(camDirection.x, 0, camDirection.z); //This line removes the "space ship" 3D flying effect. We take the cam direction but remove the y axis value
-
-    //    if (dir != Vector3.zero)
-    //    { //turn the character to face the direction of travel when there is input
-    //        transform.rotation = Quaternion.Slerp(
-    //        transform.rotation,
-    //        Quaternion.LookRotation(targetDirection),
-    //        Time.deltaTime * turnSpeed
-    //        );
-    //    }
-
-    //    body.velocity = targetDirection.normalized * speed;     //normalized prevents char moving faster than it should with diagonal input
-
-    //}
-
-    //public void HandleRotation()
-    //{
-    //    float targetRotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
-    //    Quaternion lookAt = Quaternion.Slerp(transform.rotation,
-    //                                  Quaternion.Euler(0, targetRotation, 0),
-    //                                  0.5f);
-    //    transform.rotation = lookAt;
-    //    //body.rotation = lookAt;
-
-    //}
-
+    // detect if character hits the ground, though being triggered two times: when jump starts and when it ends
     public bool isGrounded()
     {
         RaycastHit hit;
@@ -325,6 +169,7 @@ void Update()
         }
     }
 
+    // needed for coroutine cause we need to delay the jump so animation matches the jump
     IEnumerator Jump()
     {
 
